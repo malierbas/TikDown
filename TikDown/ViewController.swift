c23f21dc96d13da38ca343cc67e8c35a834abda4
Ali <63293527+LykaMali@users.noreply.github.com>
2022-05-18 11:37:40 +0300

    Initial Commit

---
+++ TikDown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikDown
+//
+//  Created by Ali on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
